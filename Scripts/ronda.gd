62b4755d4c21c9bdb8247c9e7206837b4e8461f5
tobiasflores <tobiymin@gmail.com>
2026-05-21 23:05:58 -0300

    agrego boton cincuenta-cincuenta y preguntas de USA e Inglaterra

--- Scripts/ronda.gd
+++ Scripts/ronda.gd
@@ -14,6 +14,28 @@ func mostrar_pregunta(indice: int):
 	var preguntaActual: Pregunta = cola_preguntas[indice]
 	$LabelPregunta.text = preguntaActual.enunciado
 	var opciones_mezcladas = preguntaActual.opciones.duplicate()
+	var respuesta_correcta = opciones_mezcladas[preguntaActual.indice_correcto]
 	opciones_mezcladas.shuffle()
-	for i in range($BotonesOpciones.get_child_count()):
-		$BotonesOpciones.get_child(i).text = opciones_mezcladas[i]
+	indiceActual = opciones_mezcladas.find(respuesta_correcta)
+	
+	var botones = $BotonesOpciones.get_children()
+	for i in range(botones.size()):
+		botones[i].text = opciones_mezcladas[i]
+		botones[i].disabled = false
+		botones[i].visible = true
+
+func cincuenta_cincuenta():
+	var botones = $BotonesOpciones.get_children()
+	var incorrectos = []
+	for i in range(botones.size()):
+		if i != indiceActual:
+			incorrectos.append(i)
+
+	incorrectos.shuffle()
+	botones[incorrectos[0]].visible = false
+	botones[incorrectos[1]].visible = false
+
+func _on__50_pressed():
+	cincuenta_cincuenta()
+	print("cincuenta presionado")
+	$Boton50.disabled = true
